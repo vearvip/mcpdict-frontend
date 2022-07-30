da105150c7d56f93d9d6aaef550fb1025bb83651
vear.vip@qq.com <vear.vip@qq.com>
2022-07-31 01:57:31 +0800

    sdf

---
+++ src/pages/join.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Join = () => {
+  return <div style={{
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '60vh',
+    userSelect: 'none'
+  }}>
+    🚧施工中
+  </div>
+}
+
+export default Join
