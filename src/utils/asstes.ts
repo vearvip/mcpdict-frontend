d81cf61ab05197f519fe0695da9f2a6056b98388
vear.vip@qq.com <vear.vip@qq.com>
2022-08-07 10:37:08 +0800

    3222

--- src/utils/asstes.ts
+++ src/utils/asstes.ts
@@ -1,5 +1,8 @@
 export const logo = "https://vkceyugu.cdn.bspapp.com/VKCEYUGU-20c9e8f4-2e86-42ce-9b49-841d84f433a9/a549b7dc-294f-43e7-bf89-00d4291957db.png"
 export const logoText = "https://vkceyugu.cdn.bspapp.com/VKCEYUGU-20c9e8f4-2e86-42ce-9b49-841d84f433a9/0fdcfab1-0558-4a08-991e-726e65c9b155.png"
 
-export const upyunLogo = 'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-20c9e8f4-2e86-42ce-9b49-841d84f433a9/3b9b63df-0791-42ed-a5d3-632d2bfb1789.png'
+export const upyunLogo = 'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-20c9e8f4-2e86-42ce-9b49-841d84f433a9/4790c38b-22d5-4420-86a1-51f1abfccae6.png'
+
+
+// export const upyunLogo = 'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-20c9e8f4-2e86-42ce-9b49-841d84f433a9/3b9b63df-0791-42ed-a5d3-632d2bfb1789.png'
 
