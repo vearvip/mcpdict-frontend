78089e16a695a7feed5a26882ce82f58d9804423
vear.vip@qq.com <vear.vip@qq.com>
2022-08-29 23:46:59 +0800

    232323

--- src/pages/LongSearch/index.tsx
+++ src/pages/LongSearch/index.tsx
@@ -4,15 +4,17 @@ import { logo, logoText } from "@/utils/asstes";
 import { Component, createSignal, Show } from 'solid-js'; 
 import settingPng from '@/assets/setting.svg'
 import NoData from '@/components/NoData'
+import LogoBlock from "@/components/LogoBlock";
 
 const LongSearch: Component = (props) => {
   const [loading, setLoading] = createSignal(false) 
   return <> 
     <div class={styles.search_bar}>
-      <div class={styles.logo_box}>
+      {/* <div class={styles.logo_box}>
         <img class={styles.logo} src={logo as any} />
         <img class={styles.logo_text} src={logoText as any} />
-      </div> 
+      </div>  */}
+      <div class={styles.logo_block}><LogoBlock /></div>
         <textarea placeholder="长文注音，单次只可选择一种语言，请在右侧设置按钮选择语言" maxLength={200} class={styles.textarea} />
         <div class={styles.btn_box}>
         <button class={styles.setting_btn}> 
