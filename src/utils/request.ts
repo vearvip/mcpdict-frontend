46aff6f39357d9eb356020ed3375bdd5755b43a1
vear.vip@qq.com <vear.vip@qq.com>
2024-11-30 13:54:31 +0800

    1

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,5 +1,5 @@
 let baseURL = 'https://frp-end.top:45374'
-baseURL = 'http://localhost:3000'
+// baseURL = 'http://localhost:3000'
 // @ts-ignore
 export const fetcher = (url, ...args) => fetch(
   baseURL + url,
