6a050a4f1d3e40fc07afda762c0027ed1703b52f
唯二 <vear.vip@qq.com>
2024-12-05 02:50:52 +0000

    1

--- src/utils/hooks.js
+++
@@ -1,6 +0,0 @@
-import { useSize } from "ahooks";
-
-export function useMobile() {
-  const size = useSize(document.querySelector("body"));
-  return size.width <= 1064;
-}
--- src/utils/request.js
+++
@@ -1,8 +0,0 @@
-let baseURL = 'https://api.mcpdict.vear.vip'
-// baseURL = 'http://localhost:3000'
-// @ts-ignore
-export const fetcher = (url, ...args) => fetch(
-  baseURL + url,
-  ...args
-).then((res) => res.json())
- 
