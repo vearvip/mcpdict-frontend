2393516c7a206b447db97b11c7c93f267c3ed499
vear.vip@qq.com <vear.vip@qq.com>
2024-12-12 01:36:22 +0800

    bootcdn

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,50 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 import { join } from "path";
-import { visualizer } from 'rollup-plugin-visualizer'
-import cdn from 'vite-plugin-cdn-import'
-
+import { visualizer } from "rollup-plugin-visualizer";
+// import cdn from "vite-plugin-cdn-import";
+import cdn from "@vearvip/vite-plugin-cdn-import";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     cdn({
-      modules: ['react', 'react-dom', 'dayjs', 'antd'],
+      prodUrl: 'https://cdn.bootcdn.net/ajax/libs/{name}/{version}/{path}',
+      // enableInDevMode: true,
+      modules: [
+        'react', 
+        'react-dom', 
+        'dayjs', 
+        'antd',
+      ],
     }),
+    // cdn({
+    //   enableInDevMode: true,
+    //   modules: [
+    //     {
+    //       name: "react-dom",
+    //       var: "ReactDOM",
+    //       // path: `umd/react-dom.production.min.js`,
+    //       // prodUrl: 'https://cdn.jsdelivr.net/npm/{name}@{version}/{path}',
+    //       path: 'https://cdn.bootcdn.net/ajax/libs/react-dom/18.3.1/umd/react-dom.production.min.js'
+    //     },
+    //     {
+    //       name: "react",
+    //       var: "React",
+    //       // path: `umd/react.production.min.js`,
+    //       // prodUrl: 'https://cdn.jsdelivr.net/npm/{name}@{version}/{path}',
+    //       path: 'https://cdn.bootcdn.net/ajax/libs/react/18.3.1/umd/react.production.min.js'
+    //     },
+    //   ],
+    // }),
     visualizer({
-      open: true
-    })
-
+      open: false,
+    }),
   ],
   resolve: {
     alias: {
-      '@': join(__dirname, "src"),
-    }
+      "@": join(__dirname, "src"),
+    },
   },
-})
+});
