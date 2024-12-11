49faa3f068e8841096eebce43bdbedc2a63f24a1
vear.vip@qq.com <vear.vip@qq.com>
2024-12-12 02:02:59 +0800

    搞搞

--- vite.config.js
+++ vite.config.js
@@ -9,35 +9,16 @@ import cdn from "@vearvip/vite-plugin-cdn-import";
 export default defineConfig({
   plugins: [
     react(),
-    cdn({
-      prodUrl: 'https://cdn.bootcdn.net/ajax/libs/{name}/{version}/{path}',
-      // enableInDevMode: true,
-      modules: [
-        'react', 
-        'react-dom', 
-        'dayjs', 
-        'antd',
-      ],
-    }),
     // cdn({
+    //   prodUrl: 'https://cdn.bootcdn.net/ajax/libs/{name}/{version}/{path}',
     //   enableInDevMode: true,
     //   modules: [
-    //     {
-    //       name: "react-dom",
-    //       var: "ReactDOM",
-    //       // path: `umd/react-dom.production.min.js`,
-    //       // prodUrl: 'https://cdn.jsdelivr.net/npm/{name}@{version}/{path}',
-    //       path: 'https://cdn.bootcdn.net/ajax/libs/react-dom/18.3.1/umd/react-dom.production.min.js'
-    //     },
-    //     {
-    //       name: "react",
-    //       var: "React",
-    //       // path: `umd/react.production.min.js`,
-    //       // prodUrl: 'https://cdn.jsdelivr.net/npm/{name}@{version}/{path}',
-    //       path: 'https://cdn.bootcdn.net/ajax/libs/react/18.3.1/umd/react.production.min.js'
-    //     },
+    //     'react', 
+    //     'react-dom', 
+    //     'dayjs', 
+    //     'antd',
     //   ],
-    // }),
+    // }), 
     visualizer({
       open: false,
     }),
