f6ac2a7841a566ccd338922d7d63f8d6fdb18b41
唯二 <vear.vip@qq.com>
2024-12-12 03:30:52 +0000

    首屏优化

--- vite.config.js
+++ vite.config.js
@@ -2,30 +2,79 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { join } from "path";
 import { visualizer } from "rollup-plugin-visualizer";
-// import cdn from "vite-plugin-cdn-import";
-import cdn from "@vearvip/vite-plugin-cdn-import";
+import cdn from "@vearvip/vite-plugin-cdn-import"
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    // cdn({
-    //   prodUrl: 'https://cdn.bootcdn.net/ajax/libs/{name}/{version}/{path}',
-    //   enableInDevMode: true,
-    //   modules: [
-    //     'react', 
-    //     'react-dom', 
-    //     'dayjs', 
-    //     'antd',
-    //   ],
-    // }), 
-    visualizer({
-      open: false,
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@": join(__dirname, "src"),
+// 自定义Rollup插件用于HTML处理
+function injectBaiduAnalyticsPlugin(mode) {
+  return {
+    name: 'inject-baidu-analytics',
+    async transformIndexHtml(html, ctx) {
+      const isProduction = mode === 'production'
+
+      if (isProduction) {
+        // 百度统计的 <script> 标签
+        const baiduAnalyticsScript = `
+          <script>
+            var _hmt = _hmt || [];
+            (function() {
+              var hm = document.createElement("script");
+              hm.src = "https://hm.baidu.com/hm.js?ee245bfe8773dc8395fa35a52e8f41cc";
+              var s = document.getElementsByTagName("script")[0];
+              s.parentNode.insertBefore(hm, s);
+            })();
+          </script>
+        `.trim();
+
+        // 将百度统计的 <script> 标签插入到 <!-- INJECT_SCRIPT --> 占位符处
+        html = html.replace('<!-- INJECT_SCRIPT -->', baiduAnalyticsScript);
+      }
+
+      return html;
     },
-  },
+  };
+}
+
+export default defineConfig(({ command, mode }) => {
+  return {
+    esbuild: {
+      drop: ['console', 'debugger']
+    },
+    plugins: [
+      react(),
+      cdn({
+        prodUrl: 'https://cdn.bootcdn.net/ajax/libs/{name}/{version}/{path}',
+        enableInDevMode: true,
+        modules: [
+          'react', 
+          'react-dom', 
+          // 'dayjs', 
+          // 'antd',
+        ],
+      }), 
+      visualizer({
+        open: false,
+      }),
+      injectBaiduAnalyticsPlugin(mode) // 注册自定义百度统计插件
+    ],
+    resolve: {
+      alias: {
+        "@": join(__dirname, "src"),
+      },
+    },
+    build: {
+      rollupOptions: {
+        output: { 
+          // 手动指定 chunk 分割规则
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              if (id.includes('antd')) {
+                return 'vendor-antd'; // 将 antd 相关内容放入 vendor-antd chunk
+              }
+              return 'vendor'; // 其他 node_modules 内容放入 vendor chunk
+            }
+          },
+        },
+      },
+    },
+  }
 });
