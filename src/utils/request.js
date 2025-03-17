ae6079ab39f478fe9af35c804e44c269a361746d
vear.vip@qq.com <vear.vip@qq.com>
2025-03-17 23:46:17 +0800

    恢复

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,7 +2,7 @@ import { message } from 'antd';
 import qs from 'qs';
 
 let baseURL = 'https://server.mcpdict.vear.vip'
-baseURL = 'http://localhost:3000' 
+// baseURL = 'http://localhost:3000' 
 
 // @ts-ignore
 export const fetcher = (url, ...args) => fetch(
